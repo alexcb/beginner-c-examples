44f5aa4e883dfcafddd334aed56e7fa072a97f75
Alex Couture-Beil <alex@mofo.ca>
2018-03-09 20:21:17 -0800

    error handling in scanf

--- 05_break-statement/main.c
+++ 05_break-statement/main.c
@@ -4,16 +4,21 @@ int main(int argc, char **argv)
 {
 	int total_numbers_squared = 0;
 	int number;
+	int num_read;
 
 	printf("Give me a positive number and I will square it; if the number is negative I will exit.\n");
 
 	while( 1 ) {
 		printf("enter number: ");
-		scanf("%d", &number);
+		num_read = scanf("%d", &number);
+		if( num_read != 1 ) {
+			printf("there was an error reading the number, exiting the program.\n");
+			return 1;
+		}
 
 		if( number <= 0 ) {
 			if( number == 0 ) {
-				printf("surprise! i'm going to exit even though 0 isn't really negative\n");
+				printf("surprise! i'm going to exit even though 0 isn't really negative.\n");
 			}
 			break;
 		}
@@ -22,16 +27,16 @@ int main(int argc, char **argv)
 			printf("I really like this number.\n");
 		}
 
-		printf("the squared value of %d is %d\n", number, number*number);
+		printf("the squared value of %d is %d.\n", number, number*number);
 		total_numbers_squared = total_numbers_squared + 1;
 	}
 
 	if( total_numbers_squared > 0 ) {
-		printf("together we squared %d values\n", total_numbers_squared);
+		printf("together we squared %d values.\n", total_numbers_squared);
 	} else if( total_numbers_squared == 1 ) {
-		printf("together we squared a single value\n");
+		printf("together we squared a single value.\n");
 	} else {
-		printf("we didn't square anything\n");
+		printf("we didn't square anything.\n");
 	}
 	return 0;
 }
